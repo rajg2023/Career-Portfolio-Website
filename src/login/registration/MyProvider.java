22dec22a2c5c80cd90dd3d1e319b0c4bfd8ef7f0
Raj G <rajivgiri513@gmail.com>
2023-10-14 06:08:44 -0400

    Update MyProvider.java

--- src/login/registration/MyProvider.java
+++ src/login/registration/MyProvider.java
@@ -4,8 +4,8 @@ public interface MyProvider {
 	
 	// set the postgres db url, username, and password
 	String url = "jdbc:postgresql://ec2-54-227-237-223.compute-1.amazonaws.com:5432/dde5nv2s4avttf?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
-	String username = "mdoxaevgiwnvmd";
-	String pwd = "2a871683b1140322e3311f9367702629f7ec7646c0c4f5025698c71090825cff";
+	String username = "username";
+	String pwd = "password";
 	 //Class.forName("org.postgresql.Driver");
 	// get and return connection
 	//connection = DriverManager.getConnection(url, username, password);
